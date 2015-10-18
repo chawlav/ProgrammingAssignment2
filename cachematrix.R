02070ef097352d07af3b2377565cd8c0156cbe16
Vishal Chawla <vchawla@Vishals-MacBook-Air.local>
2015-10-17 20:58:36 -0700

    comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,15 +5,24 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+
 	mat <- NULL
+	
+	#set function 
 	set <- function(y){
+		#set the value of x in the parent scope
 		x <<- y
+		#since the matrix data is changed when this function is called we must destroy the cached copy of the inverse
 		mat <<- NULL
 	}
+
+	# return the matrix data
 	get <- function() x
 
+	# return the inverse (cached)
 	getInverse <- function() mat
 
+	#cache the inverse
 	setInverse <- function(data) mat <<- data
 
 	list(set = set, get = get,  setInverse = setInverse, getInverse = getInverse)
@@ -24,14 +33,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
 
+	# get the inverse using the supplied functionin the arguments (x)
 	inv<- x$getInverse()
+	#check not null
 	if (!is.null(inv)){
 		message("cached inverse")
 		return(inv)
 	}
-	# need to compute
+	# inverse was not cached earlier hence needs to be computed
+	# get the underlying data
 	mat <- x$get()
+	#assume matrix is invertible. compute the inverse
 	inv <- solve(mat)
+	#cache it
 	x$setInverse(inv)
 	inv
        
